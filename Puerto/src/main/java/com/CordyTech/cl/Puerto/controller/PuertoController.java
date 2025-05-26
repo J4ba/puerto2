f95c4f44c6a451d7b95512e5df907028e3fd213e
Javiera <no.javiera.24@gmail.com>
2025-05-26 03:47:14 -0400

    Add files via upload

---
+++ Puerto/src/main/java/com/CordyTech/cl/Puerto/controller/PuertoController.java
@@ -0,0 +1,64 @@
+package com.CordyTech.cl.Puerto.controller;
+
+import com.CordyTech.cl.Puerto.model.Puerto;
+import com.CordyTech.cl.Puerto.service.PuertoService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/puertos")
+public class PuertoController {
+
+    @Autowired
+    private PuertoService puertoService;
+
+    @GetMapping
+    public List<Puerto> listar() {
+        return puertoService.listarPuertos();
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Puerto> obtenerPorId(@PathVariable int id) {
+        return puertoService.obtenerPuertoPorId(id)
+                .map(ResponseEntity::ok)
+                .orElse(ResponseEntity.notFound().build());
+    }
+
+    @GetMapping("/nombres")
+    public ResponseEntity<Puerto> obtenerPuertoPorNombre(@PathVariable String nombrePuerto) {
+        return puertoService.obtenerPuertoPorNombre(nombrePuerto)
+                .map(ResponseEntity::ok)
+                .orElse(ResponseEntity.notFound().build());
+    }
+
+    @PostMapping
+    public Puerto guardar(@RequestBody Puerto puerto) {
+        return puertoService.guardarPuerto(puerto);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> eliminar(@PathVariable int id) {
+        puertoService.eliminarPuerto(id);
+        return ResponseEntity.noContent().build();
+    }
+
+    @GetMapping("/disponibles")
+    public List<Puerto> getPuertosDisponibles() {
+        return puertoService.getDisponibles();
+    }
+
+    @GetMapping("/tarifas")
+    public List<Map<String, Object>> getTarifasPuertos(){
+        return puertoService.getTarifas();
+    }
+
+    @GetMapping("/{id}/tarifas")
+    public Map<String, Object> getTarifasById(@PathVariable int id){
+        return puertoService.getTarifasById(id);
+    }
+}
---
+++ Puerto/src/test/java/com/CordyTech/cl/Puerto/PuertoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.CordyTech.cl.Puerto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PuertoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
